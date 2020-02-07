7dcdc19ba83e39ec4363c68cc5925b01ee8ccb3f
Tiagoadc <49619223+tiagoadc@users.noreply.github.com>
2020-02-07 13:24:19 -0300

    nosso primeira API

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, rest) => {
+    rest.status(200).send({message: 'ola mundo esse é meu nome repositorio'})
+})
+
+app.listen(4001, ()=> {
+    console.log('API inicialida na porta 4001');
+})
